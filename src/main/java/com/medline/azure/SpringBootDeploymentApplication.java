7245642105e90f7975609cabbbe6b27bfdebb0da
Nitin Gore <ngore@medline.com>
2023-11-05 19:47:49 +0530

    added new endpoint

--- src/main/java/com/medline/azure/SpringBootDeploymentApplication.java
+++ src/main/java/com/medline/azure/SpringBootDeploymentApplication.java
@@ -2,10 +2,18 @@ package com.medline.azure;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringBootDeploymentApplication {
 
+	@GetMapping("/")
+	public String welcome() {
+		return "Welcome to fist project";
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootDeploymentApplication.class, args);
 	}
